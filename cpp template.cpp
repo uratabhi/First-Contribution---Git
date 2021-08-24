396185bd6d85de4baf711b580cecb862f3387edb
Abhi Jaiswal <waitabhi2015@gmail.com>
2021-08-24 12:35:21 +0530

    first commit

---
+++ cpp template.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int             long long
+#define lld             long double
+#define ull             unsigned long long
+#define pb              push_back
+#define ppb             pop_back
+#define mp              make_pair
+#define ff              first
+#define ss              second
+#define PI              3.141592653589793238462
+#define set_bits        __builtin_popcount
+#define all(x)          (x).begin(), (x).end()
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define testcases       int t; cin>>t; while(t--)
+#define fo(i, n)        for(int i=0; i<n; i++)
+#define Fo(i, k, n)     for(int i=k; i<n; i++)
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define endl            "\n"
+#define yes             cout<<"YES"<<endl
+#define no              cout<<"NO"<<endl
+#define fast()          ios_base::sync_with_stdio(false); cin.tie(NULL)
+
+#ifndef professor
+#define dbg(x) cerr << #x<<" "; print(x); cerr << endl;
+#else
+#define dbg(x);
+#endif
+
+
+void  print(int t) {cerr << t;}
+void  print(string t) {cerr << t;}
+void  print(char t) {cerr << t;}
+void  print(lld t) {cerr << t;}
+void  print(double t) {cerr << t;}
+void  print(ull t) {cerr << t;}
+
+
+
+template <class T, class V> void  print(pair <T, V> p);
+template <class T> void  print(vector <T> v);
+template <class T> void  print(set <T> v);
+template <class T, class V> void  print(map <T, V> v);
+template <class T> void  print(multiset <T> v);
+template <class T, class V> void  print(pair <T, V> p) {cerr << "{";  print(p.ff); cerr << ",";  print(p.ss); cerr << "}";}
+template <class T> void  print(vector <T> v) {cerr << "[ "; for (T i : v) { print(i); cerr << " ";} cerr << "]";}
+template <class T> void  print(set <T> v) {cerr << "[ "; for (T i : v) { print(i); cerr << " ";} cerr << "]";}
+template <class T> void  print(multiset <T> v) {cerr << "[ "; for (T i : v) { print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void  print(map <T, V> v) {cerr << "[ "; for (auto i : v) { print(i); cerr << " ";} cerr << "]";}
+
+
+
+
+
+void solve()
+{   
+
+        
+}
+
+
+int32_t main()
+{
+   fast();
+   
+   solve();
+
+
+return 0;
+}
